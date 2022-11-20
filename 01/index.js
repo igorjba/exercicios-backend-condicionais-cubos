8c3303c4409c6a0f4a587b644b3a59e333a14913
igorjba <igorjbahia@gmail.com>
2022-11-20 10:12:55 -0300

    :recycle: refactor: Adicionada uma outra forma de resolver.

--- 01/index.js
+++ 01/index.js
@@ -1,12 +1,40 @@
-const jogada1 = "tesoura"
-const jogada2 = "pedra"
+const jogada1 = "pedra"
+const jogada2 = "papel"
 
 if (jogada1 === jogada2) {
     console.log("empate");
-} else if (((jogada1 === "pedra") && (jogada2 === "tesoura")) || ((jogada2 === "pedra") && (jogada1 === "tesoura"))) {
-    console.log("pedra");
-} else if (((jogada1 === "pedra") && (jogada2 === "papel")) || ((jogada2 === "pedra") && (jogada1 === "papel"))) {
-    console.log("papel")
-} else if (((jogada1 === "papel") && (jogada2 === "tesoura")) || ((jogada2 === "papel") && (jogada1 === "tesoura"))) {
-    console.log("tesoura")
+} else {
+    if (jogada1 === "pedra") {
+        if (jogada2 === "tesoura") {
+            console.log("pedra");
+        } else {
+            console.log("papel");
+        }
+    } else if (jogada1 === "tesoura") {
+        if (jogada2 === "pedra") {
+            console.log("pedra");
+        } else {
+            console.log("tesoura");
+        }
+    } else {
+        //jogada2 === "pedra" ? console.log("papel") : console.log("tesoura");
+
+        if (jogada2 === "pedra") {
+            console.log("papel");
+        } else {
+            console.log("tesoura");
+        }
+    }
 }
+
+
+
+// if (jogada1 === jogada2) {
+//     console.log("empate");
+// } else if (((jogada1 === "pedra") && (jogada2 === "tesoura")) || ((jogada2 === "pedra") && (jogada1 === "tesoura"))) {
+//     console.log("pedra");
+// } else if (((jogada1 === "pedra") && (jogada2 === "papel")) || ((jogada2 === "pedra") && (jogada1 === "papel"))) {
+//     console.log("papel")
+// } else if (((jogada1 === "papel") && (jogada2 === "tesoura")) || ((jogada2 === "papel") && (jogada1 === "tesoura"))) {
+//     console.log("tesoura")
+// }
