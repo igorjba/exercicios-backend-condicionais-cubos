f7692196deb95d90d6f15681b1b545583aae2dd6
igorjba <igorjbahia@gmail.com>
2022-11-28 18:50:35 -0300

    :recycle: refactor: Deixando o código mais simples.

--- 16/index.js
+++ 16/index.js
@@ -4,12 +4,21 @@ const idadeDoAluno = 16;
 //valor verdadeiro ou falso que informa se possui responsável
 const possuiResponsavel = true;
 
-if (idadeDoAluno >= 18) {
-    console.log("Rematrícula realizada com sucesso");
-} else if (((idadeDoAluno < 18) && (idadeDoAluno > 0)) && (possuiResponsavel === false)) {
-    console.log("Não é possível fazer a rematrícula");
-} else if (((idadeDoAluno < 18) && (idadeDoAluno > 0)) && (possuiResponsavel === true)) {
-    console.log("Rematrícula realizada com sucesso");
+
+if (idadeDoAluno >= 18 || possuiResponsavel) {
+    console.log("Rematrícula realizada com sucesso")
 } else {
-    console.log("Erro");
+    console.log("Não é possível fazer a rematrícula");
 }
+
+
+
+// if (idadeDoAluno >= 18) {
+//     console.log("Rematrícula realizada com sucesso");
+// } else if (((idadeDoAluno < 18) && (idadeDoAluno > 0)) && (possuiResponsavel === false)) {
+//     console.log("Não é possível fazer a rematrícula");
+// } else if (((idadeDoAluno < 18) && (idadeDoAluno > 0)) && (possuiResponsavel === true)) {
+//     console.log("Rematrícula realizada com sucesso");
+// } else {
+//     console.log("Erro");
+// }
