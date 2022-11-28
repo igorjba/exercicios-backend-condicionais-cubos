3df45f46069e3f699ccf4de3a9d0c2507a786b2c
igorjba <igorjbahia@gmail.com>
2022-11-28 17:05:16 -0300

    :recycle: refactor: Deixando mais fácil de ser lido.

--- 10/index.js
+++ 10/index.js
@@ -1,33 +1,117 @@
-const caractere = "E";
+const caractere = "1";
 
-const vogaisMaiusculas = ["A", "E", "I", "O", "U", "Y"];
-const vogaisMinusculas = ["a", "e", "i", "o", "u", "y"];
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-const consoantesMaiusculas = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "X", "W", "Z"];
-const consoantesminusculas = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "x", "w", "z"];
+if (
+    caractere === "A" ||
+    caractere === "E" ||
+    caractere === "I" ||
+    caractere === "O" ||
+    caractere === "U" ||
+    caractere === "Y"
+) {
+    console.log("Vogal maiúscula");
+} else if (
+    caractere === "a" ||
+    caractere === "e" ||
+    caractere === "i" ||
+    caractere === "o" ||
+    caractere === "u" ||
+    caractere === "y"
+) {
+    console.log("Vogal minúscula");
+} else if (
+    caractere == 1 ||
+    caractere == 2 ||
+    caractere == 3 ||
+    caractere == 4 ||
+    caractere == 5 ||
+    caractere == 6 ||
+    caractere == 7 ||
+    caractere == 8 ||
+    caractere == 9 ||
+    caractere == 0
+) {
+    console.log("Número");
+} else if (
+    caractere === "B" ||
+    caractere === "C" ||
+    caractere === "D" ||
+    caractere === "F" ||
+    caractere === "G" ||
+    caractere === "H" ||
+    caractere === "J" ||
+    caractere === "K" ||
+    caractere === "L" ||
+    caractere === "M" ||
+    caractere === "N" ||
+    caractere === "P" ||
+    caractere === "Q" ||
+    caractere === "R" ||
+    caractere === "S" ||
+    caractere === "T" ||
+    caractere === "V" ||
+    caractere === "X" ||
+    caractere === "W" ||
+    caractere === "Z"
+) {
+    console.log("Consoante maiúscula");
+} else if (
+    caractere === "b" ||
+    caractere === "c" ||
+    caractere === "d" ||
+    caractere === "f" ||
+    caractere === "g" ||
+    caractere === "h" ||
+    caractere === "j" ||
+    caractere === "k" ||
+    caractere === "l" ||
+    caractere === "m" ||
+    caractere === "n" ||
+    caractere === "p" ||
+    caractere === "q" ||
+    caractere === "r" ||
+    caractere === "s" ||
+    caractere === "t" ||
+    caractere === "v" ||
+    caractere === "x" ||
+    caractere === "w" ||
+    caractere === "z"
+) {
+    console.log("Consoante minúscula");
+};
 
-for (let vogalMaiuscula of vogaisMaiusculas) {
-    if (vogalMaiuscula === caractere) {
-        console.log("Vogal maiúscula");
-    }
-}
-for (let vogalMinuscula of vogaisMinusculas) {
-    if (vogalMinuscula === caractere) {
-        console.log("Vogal minúscula");
-    }
-}
-for (let numero of numeros) {
-    if (numero == caractere) {
-        console.log("Número");
-    }
-}
-for (let consoanteMaiuscula of consoantesMaiusculas) {
-    if (consoanteMaiuscula === caractere) {
-        console.log("Consoante maiúscula");
-    }
-}
-for (let consoanteMinuscula of consoantesminusculas) {
-    if (consoanteMinuscula === caractere) {
-        console.log("Consoante maiúscula");
-    }
-}
+
+
+
+
+
+// const vogaisMaiusculas = ["A", "E", "I", "O", "U", "Y"];
+// const vogaisMinusculas = ["a", "e", "i", "o", "u", "y"];
+// const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+// const consoantesMaiusculas = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "X", "W", "Z"];
+// const consoantesminusculas = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "x", "w", "z"];
+
+// for (let vogalMaiuscula of vogaisMaiusculas) {
+//     if (vogalMaiuscula === caractere) {
+//         console.log("Vogal maiúscula");
+//     }
+// }
+// for (let vogalMinuscula of vogaisMinusculas) {
+//     if (vogalMinuscula === caractere) {
+//         console.log("Vogal minúscula");
+//     }
+// }
+// for (let numero of numeros) {
+//     if (numero == caractere) {
+//         console.log("Número");
+//     }
+// }
+// for (let consoanteMaiuscula of consoantesMaiusculas) {
+//     if (consoanteMaiuscula === caractere) {
+//         console.log("Consoante maiúscula");
+//     }
+// }
+// for (let consoanteMinuscula of consoantesminusculas) {
+//     if (consoanteMinuscula === caractere) {
+//         console.log("Consoante maiúscula");
+//     }
+// }
