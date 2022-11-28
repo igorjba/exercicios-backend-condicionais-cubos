cea35d554e705bb51ad868aeaa9d6170c3e13094
igorjba <igorjbahia@gmail.com>
2022-11-28 17:21:31 -0300

    :recycle: refactor: Deixando o código mais simples.

--- 13/index.js
+++ 13/index.js
@@ -1,23 +1,35 @@
 //tipo de pagamento (dinheiro, credito, debito, cheque).
-const tipoDePagamento = "cheque";
+const tipoDePagamento = "credito";
 
 //valor da mercadoria (centavos)
 const valorDoProduto = 13000;
 
-const descontoCredito = valorDoProduto * (5 / 100);
-const descontoCheque = valorDoProduto * (3 / 100);
-const descontoAVista = valorDoProduto * (10 / 100);
-
-const valorFinalCredito = (valorDoProduto - descontoCredito).toFixed(2);
-const valorFinalCheque = (valorDoProduto - descontoCheque).toFixed(2);
-const valorFinalAVista = (valorDoProduto - descontoAVista).toFixed(2);
+let valorComDesconto = 0;
 
 if (tipoDePagamento === "credito") {
-    console.log(`Valor a ser pago: R$${valorFinalCredito}`);
+    valorComDesconto = valorDoProduto * 95 / 100;
 } else if (tipoDePagamento === "cheque") {
-    console.log(`Valor a ser pago: R$${valorFinalCheque}`);
-} else if ((tipoDePagamento === "dinheiro") || (tipoDePagamento === "debito")) {
-    console.log(`Valor a ser pago: R$${valorFinalCheque}`);
+    valorComDesconto = valorDoProduto * 97 / 100;
 } else {
-    console.log("Erro")
+    valorComDesconto = valorDoProduto * 90 / 100;
 }
+
+console.log(`Valor a ser pago: R$${(valorComDesconto / 100).toFixed(2)}`);
+
+// const descontoCredito = valorDoProduto * (5 / 100);
+// const descontoCheque = valorDoProduto * (3 / 100);
+// const descontoAVista = valorDoProduto * (10 / 100);
+
+// const valorFinalCredito = (valorDoProduto - descontoCredito).toFixed(2);
+// const valorFinalCheque = (valorDoProduto - descontoCheque).toFixed(2);
+// const valorFinalAVista = (valorDoProduto - descontoAVista).toFixed(2);
+
+// if (tipoDePagamento === "credito") {
+//     console.log(`Valor a ser pago: R$${(valorFinalCredito / 100).toFixed(2)}`);
+// } else if (tipoDePagamento === "cheque") {
+//     console.log(`Valor a ser pago: R$${(valorFinalCheque / 100).toFixed(2)}`);
+// } else if ((tipoDePagamento === "dinheiro") || (tipoDePagamento === "debito")) {
+//     console.log(`Valor a ser pago: R$${(valorFinalCheque / 100).toFixed(2)}`);
+// } else {
+//     console.log("Erro")
+// }
